f98477fa16875797f0a5cd062dd40415dd0959a9
gbf96 <botelhogabriel09@gmail.com>
2025-03-24 13:23:12 +0000

    usbport

--- RouletteGameVhdl/RouletteGame.vhd
+++ RouletteGameVhdl/RouletteGame.vhd
@@ -7,10 +7,7 @@ entity RouletteGame is
 	LIN: in std_logic_vector(3 downto 0);
 	COL: out std_logic_vector(3 downto 0);
 	CLK: in std_logic;
-	ACK: in std_logic;
-	RESET   : in std_logic;
-	Q: out std_logic_vector(3 downto 0);
-	Dval: out std_logic
+	RESET   : in std_logic
 	);
 end entity;
 
@@ -27,17 +24,35 @@ component KeyboardReader
 		Dval: out std_logic
 		);
 end component;
+
+component UsbPort 
+	PORT
+	(
+		inputPort:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
+		outputPort :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+	);
+END component;
+
+signal Qout: std_logic_vector(3 downto 0);
+signal Dvalout: std_logic;
+signal ACKout: std_logic;
     
 begin
 
 KeyboardReader_inst: KeyboardReader port map(
 	LIN => LIN,
 	COL => COL,
-	Q => Q,
+	Q => Qout,
 	CLK => CLK,
-	ACK => ACK,
+	ACK => ACKout,
 	RESET => RESET,
-	Dval => Dval
+	Dval => Dvalout
+);
+
+UsbPort_inst: UsbPort port map(
+	inputPort(3 downto 0) => Qout,
+	inputPort(4) => Dvalout,
+	outputPort(4) => ACKout
 );
 
 end structural;
